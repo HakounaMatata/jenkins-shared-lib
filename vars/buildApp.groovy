ebb551ed4ea77275aacba9fb258d5f975404bf1f
AbdeIkader <abdelrahmanabdelkader2002@gmail.com>
2025-11-01 17:26:15 +0200

    Fixed buildApp Error

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -1,7 +1,5 @@
 def call() {
     echo "⚙️ Building Java project from shared library..."
-    dir('python-iti') { 
-        sh 'mvn -q -DskipTests=true clean package'
-        sh 'cp target/demo1-0.0.1-SNAPSHOT.jar target/app.jar'
-    }
+    sh 'mvn -q -DskipTests=true clean package'
+    sh 'cp target/demo1-0.0.1-SNAPSHOT.jar target/app.jar'
 }
