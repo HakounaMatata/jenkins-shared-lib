9a38db4210b04bd531b5add26fe6d75662e76e1d
AbdeIkader <abdelrahmanabdelkader2002@gmail.com>
2025-11-01 17:16:39 +0200

    Fiexed Issue

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -1,7 +1,7 @@
 def call() {
     echo "⚙️ Building Java project from shared library..."
-    sh '''
-        mvn -q -DskipTests=true clean package
-        cp target/demo1-0.0.1-SNAPSHOT.jar target/app.jar
-    '''
+    dir('python-iti') { 
+        sh 'mvn -q -DskipTests=true clean package'
+        sh 'cp target/demo1-0.0.1-SNAPSHOT.jar target/app.jar'
+    }
 }
